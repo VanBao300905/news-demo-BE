4230fa023f523903a284d06a4c43235b485403b9
phuckk05 <phuckk2101@gmail.com>
2024-11-08 07:09:21 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/** the name of the databas */
+define ('DB_NAME ' , 'news' );
+/** MySQL database username */
+define ('DB_USER' , 'root');
+/** My SQL database password */
+define( 'DB_PASSWORD' , '');
+/**MySQL hostname */
+define('DB_HOST' , 'localhost');
+/** port number of DB */
+define( 'PORT' , '3306');
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
